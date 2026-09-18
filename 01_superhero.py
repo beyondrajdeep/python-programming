c999aa3bb8a2a88ded6d5e49235a59a12cd8a01c
Rajdeep Mandal <rajdeepmandal2026@gmail.com>
2026-09-18 19:27:52 +0530

    Add files via upload

---
+++ 01_superhero.py
@@ -0,0 +1,19 @@
+#tony's details in variables
+first_name= "Tony"
+last_name= "Stark"
+age= 53
+height= 1.85
+is_superhero= True
+
+#ask the user for tony's superhero name
+superhero_name = input("Enter Tony's superhero name: ")
+
+#print all the details
+print("------Personal details------")
+print("first name:",first_name)
+print("last name:",last_name)
+print("age:",age)
+print("height:",str(height)+"m") #for no space between 1.83 and m
+#print("height:",height,"m")
+print("Is a superhero:", is_superhero)
+print("Superhero name:", superhero_name)
