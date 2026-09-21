fcd9c393ef0a8e3b28678492dc67c93c938116d2
Rajdeep Mandal <rajdeepmandal2026@gmail.com>
2026-09-21 23:38:12 +0530

    Add files via upload

---
+++ sum.py
@@ -0,0 +1,10 @@
+# Program to add two numbers
+# Taking input from user
+num1= float(input("Enter first number: "))
+num2= float(input("Enter second number: "))
+
+# Calculating sum
+sum_result = num1+num2
+
+# Displaying result
+print("The sum is:", sum_result)
